2c47d807f275f54c78a6b0c25148e0a2b05348c1
aramazani <aramazani100@gmail.com>
2024-09-27 23:32:29 -0500

    Rename

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "flowcrmtutorial")
+@Theme(value = "flowcrm")
 public class  Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
